389baf8f0a7678acfde76ef413ddb2f6caa142bd
defel <defel@gmx.de>
2015-02-07 13:54:33 +0100

    add functionality and update README.md

--- README.md
+++ README.md
@@ -2,3 +2,63 @@ hapi-setup-helpers
 ==================
 
 hapi helpers for app setup
+
+
+pluginLoader()
+--------------
+
+a helper method to allow loading plugins in application start synchronous with `async.series` for example.
+
+Usage:
+
+```js
+async.series([
+  // load hapi-auth-cookie plugin
+  helper.pluginLoader(server, 'hapi-auth-cookie', {}, function(next) {
+    server.auth.strategy('session', 'cookie', {
+      password: 'i wear no pants',
+      cookie: 'tbsid',
+      redirectTo: '/login',
+      isSecure: false
+    });
+    next();
+  }),
+
+  // .. load more plugins
+])
+```
+
+pluginErrorAsyncCallback()
+--------------------------
+
+Small helper to print error message on failures, or ok on success, print out colors.
+
+loadRouteTable()
+----------------
+
+Helper to load Routes as a table. 
+
+One line has the following fields:
+
+- Method(s)
+- Path
+- Handler
+- Target for handler
+- Is this route protected?
+
+Usage:
+```js
+helper.loadRouteTable(server, [
+    // file handler
+    ["GET",           "/",                     "file",      "index.html",      true],
+    // javascript handler, supports with multiple methods
+    [["GET","POST"],  "/login",                "handler",   "login",           "login"],
+    // last parameter indicates that this resource is protected 
+    ["GET",           "/logout",               "handler",   "logout",          true],
+    // directory handler
+    ["GET",           "/{param*}",             "directory", "./",              false],
+    // toothache handler
+    ["GET",           "/api/contacts",         "toothache", "Contacts.find",   true]
+    // load more routes
+]);
+```
---
+++ index.js
@@ -0,0 +1,114 @@
+var chalk = require('chalk');
+
+exports.pluginLoader = function(server, name, opts, doneHandler) {
+  return function(next) {
+    server.register({
+      register: require(name), 
+      options: opts
+    }, exports.pluginErrorAsyncCallback(name, doneHandler, next)
+    );
+  };
+}
+
+exports.pluginErrorAsyncCallback = function(name, cb, next) {
+  var name = chalk.white.bgBlack.bold(name);
+
+  console.log( chalk.white.bgYellow.bold('[load] '), name, ' ...');
+
+  return function(err) {
+    if(err) {
+      console.log( chalk.white.bgRed.bold('[error] '), name, ' ...');
+      console.error(err); 
+      throw err;
+    }
+    console.log( chalk.white.bgGreen.bold('[done] '), name, ' ...');
+    cb(next);
+  }
+}
+
+var routeTableMap = {
+  METHOD:0,
+  PATH:1,
+  BACKEND: 2,
+  ARG: 3,
+  AUTH: 4
+};
+
+exports.loadRouteTable = function(server, routeTable) {
+  var routes = [];
+
+  routeTable.forEach(function(routeEntry) {
+    var route = {
+      config: {}
+    };
+
+    console.log(
+      chalk.white.bgGreen.bold('[route]'), 
+      chalk.white.bgBlack.bold(routeEntry[routeTableMap.METHOD] + " " + routeEntry[routeTableMap.PATH])
+    );
+
+    route.method = routeEntry[routeTableMap.METHOD];
+    route.path = routeEntry[routeTableMap.PATH];
+
+    switch(typeof routeEntry[routeTableMap.AUTH]) {
+      case "string": 
+        switch(routeEntry[routeTableMap.AUTH]) {
+          case "login":
+            route.config.auth = {
+              mode: 'try',
+              strategy: 'session'
+            };
+            
+            route.config.plugins = route.config.plugins || {};
+            route.config.plugins['hapi-auth-cookie'] = {
+              redirectTo: false
+            };
+
+            break;
+        }
+        break;
+      case "boolean":
+        if(routeEntry[routeTableMap.AUTH]) {
+          route.config.auth = 'session';
+        }
+        break;
+    }
+
+    switch(routeEntry[routeTableMap.BACKEND]) {
+      case 'file': 
+        route.config.handler = {
+          file: routeEntry[routeTableMap.ARG]
+        };
+        break;
+      case 'directory':
+        route.config.handler = {
+          directory: {
+            path: routeEntry[routeTableMap.ARG]
+          }
+        }
+        break;
+      case 'handler':
+        var path = __dirname + '/../src/handlers/' + routeEntry[routeTableMap.ARG] + '.js';
+        route.config.handler = require(path);
+
+        break;
+      case 'toothache':
+        var
+          argParts = routeEntry[routeTableMap.ARG].split('.'),
+          model = argParts[0],
+          func = argParts[1],
+          path = __dirname + '/../src/models/' + model.toLowerCase() + '.js',
+          Model = require(path);
+
+        route.config.handler = Model(server)[func];
+
+        break;
+      default:
+        throw new Error('unknown backend: ' + routeEntry[routeTableMap.BACKEND]);
+    }
+
+    routes.push(route);
+  });
+
+  server.route(routes);
+}
